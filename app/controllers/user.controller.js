1ae6aa879dd9ac1a08dcff7bbf61c8af1e45e90f
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-03-19 15:39:43 +0100

    GET method for list of users

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,23 +1,33 @@
 var User = require('../models/user.model');
 
-module.exports = {
+ var newUser = {
 
-  addUser: function(req, res ) {
+  addUser: function(req, res, next) {
 
-    var newUser = {
+    var userObj = {
       username: req.body.username,
       password: req.body.password,
       secret: req.body.secret
     };
 
-
-
-    User.create( newUser, function(err, data) {
+    User.create(userObj, function(err, data) {
       if (err) {
         res.send(err);
       }
-
       res.json(data);
+
+      next();
     });
+  },
+
+  listUsers: function(req, res) {
+    User.find(function(err, Users){
+      if (err) {
+        res.send(err);
+      }
+      res.json(Users);
+    })   
   }
 }
+
+module.exports = newUser;
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,17 +1,13 @@
 var express = require('express');
 
-var User = require('../controllers/user.controller');
-
 var router = express.Router();
+
+var User = require('../controllers/user.controller');
 
 //create/register new user
 router.post('/user/new', User.addUser);
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/user', function(req, res) {
-  res.json({
-    message: 'hooray! welcome to our api!'
-  });
-});
+// get list of users registered in the database
+router.get('/users', User.listUsers);
 
 module.exports = router;
