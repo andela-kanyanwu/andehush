c643978d35e412e81c43ff4e7e278f18ec154916
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-03-19 17:26:49 +0100

    GET method for a single user by ID

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -5,9 +5,12 @@ var router = express.Router();
 var User = require('../controllers/user.controller');
 
 //create/register new user
-router.post('/user/new', User.addUser);
+router.post('/users/new', User.addUser);
 
 // get list of users registered in the database
 router.get('/users', User.listUsers);
+
+//get a single user
+router.get('/users/:user_id', User.getUserInfo);
 
 module.exports = router;
