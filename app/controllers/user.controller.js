cf36feb1e2a8578ac6d229a21ed95ddf93b5030e
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-03-20 03:17:33 +0100

    PUT and DELETE methods for user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,6 +1,9 @@
 var User = require('../models/user.model');
 
- var newUser = {
+// // Create a new instance of the User model
+// var user = new User();
+
+var newUser = {
 
   addUser: function(req, res, next) {
 
@@ -14,30 +17,100 @@ var User = require('../models/user.model');
       if (err) {
         res.send(err);
       }
-      res.json(data);
 
+      else{
+        res.json(data);
+      } 
       next();
     });
   },
 
-  listUsers: function(req, res) {
-    User.find(function(err, data){
+  listUsers: function(req, res, next) {
+
+    User.find(function(err, data) {
       if (err) {
         res.send(err);
       }
-      res.json(data);
-    });   
+
+      else{
+        res.json(data);
+      }
+      next();
+    });
   },
 
-  getUserInfo: function(req, res) {
-    User.findById(req.params.user_id, function(err, data){
+  getUserInfo: function(req, res, next) {
+
+    User.findById(req.params.user_id, function(err, data) {
       if (err) {
         res.send(err);
       }
-      res.json(data);
+
+      else{
+        res.json(data);
+      } 
+      next();
+    });
+  },
+
+  editUserInfo: function(req, res, next) {
+
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) {
+        res.send(err);
+      }
+
+      //check if user does not exist
+      else if (user === null) {
+        res.json({message: 'User does not exist'})
+      }
+
+      //update all user info
+      else{
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.secret = req.body.secret;
+
+        //save the user info
+        user.save(function(err) {
+          if (err) {
+            res.send(err);
+          }
+
+          else{
+            res.json(user);
+          } 
+          next();
+        });
+      }    
+    });
+  },
+
+  deleteUser: function(req, res, next) {
+
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) {
+        res.send(err);
+      }
+
+      //check if user does not exist
+      else if (user === null) {
+        res.json({message: 'User does not exist'})
+      }
+
+      //else delete the user
+      else {
+        user.remove(function(err){
+          if (err) {
+            res.send(err);
+          }
+
+          res.json({ message: 'Successfully deleted' });
+          next();
+        });
+      }
     });
   }
-
 }
 
 module.exports = newUser;
