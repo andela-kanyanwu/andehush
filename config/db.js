89361903d88c50a4dea18f7c3fc1f6a41bbf21b7
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-03-26 23:13:40 +0100

    refactored the database url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   localUrl: 'mongodb://localhost/hushDatabase',
-  productionUrl: 'mongodb://kosy:Kosy2012.@ds041337.mongolab.com:41337/andehushdehush'
+  productionUrl: 'mongodb://kosy:Kosy2012.@ds041337.mongolab.com:41337/andehush'
 }
